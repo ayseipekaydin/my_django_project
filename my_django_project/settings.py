c0e89a5b0b29fae09b13ca73ca07063199931a35
Ayşe İpek Aydın <ayseipekaydin2004@gmail.com>
2026-05-07 23:17:14 +0300

    feat: finalize docker setup and env variables

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -11,12 +11,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, 'docker.env'))
 
 
 # Quick-start development settings - unsuitable for production
